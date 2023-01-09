d420ad0e41bc466ecf973e1c34e5ab0bd8340a8e
koaqu <1127911738@qq.com>
2023-01-09 17:09:14 +0800

    commit

---
+++ src/main/java/JavaTest.java
@@ -0,0 +1,11 @@
+/**
+ * Description 功能描述
+ * Create by hfli11 on 2023/1/9 17:00
+ */
+public class JavaTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/scala/ScalaTest.scala
@@ -0,0 +1,11 @@
+/**
+ * Description 功能描述
+ * Create by hfli11 on 2023/1/9 17:00
+ */
+object ScalaTest {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
